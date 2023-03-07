5faa3d49f1ef19c8728ea5e87878df08b7d93c6f
pkamath07 <127076454+pkamath07@users.noreply.github.com>
2023-03-07 11:12:05 -0800

    Dev prasad (#7)
    * committing platform class and datasets
    
    committing platform class and datasets
    
    * Added scripts for streamlit application
    
    Added scripts for streamlit application
    
    * updated streamlit app with drop down selection
    
    updated streamlit app with drop down selection
    
    * Integrated nested filtering into streamlit app
    
    Integrated nested filtering into streamlit app
    
    * action items + comments
    
    action items + comments
    
    * data cleanup for instagram
    
    data cleanup for instagram
    
    * renamed files
    
    renamed files
    
    * remove extra files
    
    * removing unused file
    
    * Adding dataset changes  (#4) (#5)
    
    * committing platform class and datasets
    
    committing platform class and datasets
    
    * Added scripts for streamlit application
    
    Added scripts for streamlit application
    
    * updated streamlit app with drop down selection
    
    updated streamlit app with drop down selection
    
    * Integrated nested filtering into streamlit app
    
    Integrated nested filtering into streamlit app
    
    * action items + comments
    
    action items + comments
    
    * data cleanup for instagram
    
    data cleanup for instagram
    
    * renamed files
    
    renamed files
    
    * remove extra files
    
    * removing unused file
    
    ---------
    
    Co-authored-by: bgarg96 <126200649+bgarg96@users.noreply.github.com>
    
    * Committing cleaned TikTok dataset
    
    Committing cleaned TikTok dataset
    
    * Deleting june dataset
    
    Deleting june dataset
    
    * Youtube cleaned up
    
    Youtube cleaned up
    
    * changes to include filter sets for numerics and categories
    
    changes to include filter sets for numerics and categories, changed from drop nan to fill nan with 'other' values
    
    * Added default selection to selectbox
    
    Added default selection to selectbox
    
    * commented out test code
    
    commented out test code
    
    ---------
    
    Co-authored-by: bgarg96 <126200649+bgarg96@users.noreply.github.com>

--- src/streamlit_app.py
+++ src/streamlit_app.py
@@ -1,30 +1,27 @@
 import platforms as pt
 import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
 
-'''
-This file contains the front end code for
-visualization on a webpage using Streamlit
-'''
 
 # streamlit configuration#
 
 
-instagram = pt.Social(
-    "./data/Instagram/social media influencers - instagram sep-2022.csv")
-st.write("Top 10 influencers")
-st.write(instagram.find_topn_influencers(
-    instagram.df, 10)[instagram.metrics[0]])
+instagram = pt.Social("../data/Youtube/Youtube_Dec.csv")
+# st.write("Top 10 influencers")
+# st.write(instagram.find_topn_influencers(
+#     instagram.df, 10)[instagram.metrics[0]])
 
-st.write("Top 10 influencers in the US")
-st.write(instagram.find_topn_influencers(instagram.get_subcategory_items(
-    instagram.df,
-    'Audience country',
-    'United States'),
-    10)[instagram.metrics[0]])
+# st.write("Top 10 influencers in the US")
+# st.write(instagram.find_topn_influencers(instagram.get_subcategory_items(
+#     instagram.df,
+#     'Country',
+#     'United States'),
+#     10)[instagram.metrics[0]])
 
 
 option = st.selectbox(
-    'Find top Influencers in a category', instagram.categories)
+    'Find top Influencers in a category', instagram.filters)
 
 # get top influencers across a sub-category based on subsrcibers
 filter_product = instagram.get_topn_influencers_categorical(
@@ -32,11 +29,16 @@ filter_product = instagram.get_topn_influencers_categorical(
 st.write("Top influencers for each product based on subscribers")
 st.write(filter_product)
 
-option1 = st.selectbox('Choose category', instagram.categories)
+option1 = st.selectbox('Choose category', instagram.filters)
 option2 = st.selectbox('Find top Influencers in these subcategories',
                        instagram.get_category_items(option1))
+
+# list of N's for top influencers
+topn= [1, 3, 5, 10]
+# default value of N to be displayed in selection box
+default_topn=topn.index(3)
 option3 = st.selectbox(
-    'Select Number of Influencers to display', (1, 3, 5, 10))
+    'Select Number of Influencers to display',topn,index=default_topn)
 option4 = st.selectbox('Select Metric', instagram.metrics)
 
 # get top influencers across a sub-category based on subsrcibers
@@ -44,4 +46,12 @@ subframe = instagram.get_subcategory_items(instagram.df, option1, option2)
 dfs = instagram.find_topn_influencers(subframe, option3)[option4]
 st.write("Top "+str(option3)+" influencers for "+option2 +
          " under " + option1 + " based on " + option4)
-st.write(dfs)
+# st.write(pd.DataFrame(zip(dfs['AccountName'],dfs[option4]),columns=['Account Name',option4]))
+
+plt.rcdefaults()
+fig, ax = plt.subplots()
+y_pos=range(len(dfs['AccountName']))
+ax.barh(dfs['AccountName'], dfs[option4], align='center')
+ax.invert_yaxis()  # labels read top-to-bottom
+ax.set_xlabel("No. of "+option4)
+st.pyplot(fig)
