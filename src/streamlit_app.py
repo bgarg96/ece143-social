85ba791b53867b53d829d9d53c87d308e7258780
pkamath07 <pkamath@ucsd.edu>
2023-03-07 14:09:52 -0800

    Added platform selection option
    Added platform selection option

--- src/streamlit_app.py
+++ src/streamlit_app.py
@@ -5,9 +5,14 @@ import matplotlib.pyplot as plt
 
 
 # streamlit configuration#
+PLATFORMS=['Instagram', 'Youtube', 'TikTok']
+MONTHS=['Sep','Oct','Nov','Dec']
+option_platforms = st.selectbox(
+    'Select a platform',PLATFORMS )
+option_months=st.selectbox(
+    'Select a month', MONTHS )
 
-
-instagram = pt.Social("../data/Youtube/Youtube_Dec.csv")
+instagram = pt.Social("../data/"+option_platforms+"/"+option_platforms+"_"+option_months+".csv")
 # st.write("Top 10 influencers")
 # st.write(instagram.find_topn_influencers(
 #     instagram.df, 10)[instagram.metrics[0]])
@@ -19,20 +24,31 @@ instagram = pt.Social("../data/Youtube/Youtube_Dec.csv")
 #     'United States'),
 #     10)[instagram.metrics[0]])
 
-
+# example visualization 1
+optionx_combo=instagram.filters
 option = st.selectbox(
-    'Find top Influencers in a category', instagram.filters)
+    'Find top Influencers in a category',optionx_combo )
 
 # get top influencers across a sub-category based on subsrcibers
+optionx = st.selectbox('Select Metric: ', instagram.metrics)
 filter_product = instagram.get_topn_influencers_categorical(
-    option, instagram.metrics[0])
-st.write("Top influencers for each product based on subscribers")
+    option, optionx)
+st.write("Top influencers for each "+ option +" based on subscribers")
 st.write(filter_product)
+plt.rcdefaults()
+fig, ax = plt.subplots()
+ax.barh(filter_product[option], filter_product[optionx], align='center')
+ax.invert_yaxis()  # labels read top-to-bottom
+ax.set_xlabel("No. of "+optionx)
+plt.xticks(rotation=90)
+st.pyplot(fig)
 
+
+
+# example visualization 2
 option1 = st.selectbox('Choose category', instagram.filters)
 option2 = st.selectbox('Find top Influencers in these subcategories',
                        instagram.get_category_items(option1))
-
 # list of N's for top influencers
 topn= [1, 3, 5, 10]
 # default value of N to be displayed in selection box
