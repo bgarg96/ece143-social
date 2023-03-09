780e2da69823bfe86cdfd53de96c4c759e5b05e6
pkamath07 <127076454+pkamath07@users.noreply.github.com>
2023-03-08 18:08:32 -0800

    Dev prasad2 (#10)
    * committing platform class and datasets
    
    committing platform class and datasets
    
    * Added scripts for streamlit application
    
    Added scripts for streamlit application
    
    * updated streamlit app with drop down selection
    
    updated streamlit app with drop down selection
    
    * Integrated nested filtering into streamlit app
    
    Integrated nested filtering into streamlit app
    
    * action items + comments
    
    action items + comments
    
    * data cleanup for instagram
    
    data cleanup for instagram
    
    * renamed files
    
    renamed files
    
    * remove extra files
    
    * removing unused file
    
    * Adding dataset changes  (#4) (#5)
    
    * committing platform class and datasets
    
    committing platform class and datasets
    
    * Added scripts for streamlit application
    
    Added scripts for streamlit application
    
    * updated streamlit app with drop down selection
    
    updated streamlit app with drop down selection
    
    * Integrated nested filtering into streamlit app
    
    Integrated nested filtering into streamlit app
    
    * action items + comments
    
    action items + comments
    
    * data cleanup for instagram
    
    data cleanup for instagram
    
    * renamed files
    
    renamed files
    
    * remove extra files
    
    * removing unused file
    
    ---------
    
    Co-authored-by: bgarg96 <126200649+bgarg96@users.noreply.github.com>
    
    * Committing cleaned TikTok dataset
    
    Committing cleaned TikTok dataset
    
    * Deleting june dataset
    
    Deleting june dataset
    
    * Youtube cleaned up
    
    Youtube cleaned up
    
    * changes to include filter sets for numerics and categories
    
    changes to include filter sets for numerics and categories, changed from drop nan to fill nan with 'other' values
    
    * Added default selection to selectbox
    
    Added default selection to selectbox
    
    * commented out test code
    
    commented out test code
    
    * Added platform selection option
    
    Added platform selection option
    
    * changes to include month and merge resolution
    
    changes to include month and merge resolution
    
    ---------
    
    Co-authored-by: bgarg96 <126200649+bgarg96@users.noreply.github.com>

--- src/config.py
+++ src/config.py
@@ -2,11 +2,12 @@ from typing import List, Set
 
 import pandas as pd
 
+PLATFORMS=['Instagram', 'Youtube', 'TikTok']
 MONTHS: List[str] = ['Sep', 'Oct', 'Nov', 'Dec']
 PRIMARY_KEY: str = 'AccountName'
 MONTHS_DTYPE: pd.CategoricalDtype = pd.CategoricalDtype(
     categories=MONTHS, ordered=True)
-NUMERIC_COLUMNS: Set[str] = {'Subscribers', 'AuthenticEngagement',
+METRICS: Set[str] = {'Subscribers', 'AuthenticEngagement',
                              'EngagementAverage',
                              'Views', 'Likes', 'Comments', 'Shares'}
 NAME: str = 'Name'
--- src/streamlit_app.py
+++ src/streamlit_app.py
@@ -1,55 +1,68 @@
 import matplotlib.pyplot as plt
 import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+
 
 import platforms as pt
-from config import FILTERS, PRIMARY_KEY, TOP_N
+from config import FILTERS, PRIMARY_KEY, TOP_N, MONTHS, METRICS, PLATFORMS
 
-# streamlit configuration#
 
+
+    
 if __name__ == '__main__':
+    
+    # streamlit configuration#
+    option_platforms = st.selectbox(
+        'Select a platform',PLATFORMS )
+    option_months=st.selectbox(
+        'Select a month', MONTHS )
+    
+    platform = pt.Social(option_platforms)
 
-    instagram = pt.Social("Instagram")
-    # st.write("Top 10 influencers")
-    # st.write(instagram.find_topn_influencers(
-    #     instagram.df, 10)[instagram.metrics[0]])
-
-    # st.write("Top 10 influencers in the US")
-    # st.write(instagram.find_topn_influencers(instagram.get_subcategory_items(
-    #     instagram.df,
-    #     'Country',
-    #     'United States'),
-    #     10)[instagram.metrics[0]])
-
+    # example visualization 1
+    optionx_combo=FILTERS
     option = st.selectbox(
-        'Find top Influencers in a category', FILTERS)
+        'Find top Influencers in a category',optionx_combo )
 
     # get top influencers across a sub-category based on subsrcibers
-    filter_product = instagram.get_topn_influencers_categorical(
-        option, list(instagram.metrics)[0])
-    st.write("Top influencers for each product based on subscribers")
+    optionx = st.selectbox('Select Metric: ', platform.metrics)
+    filter_product = platform.get_topn_influencers_categorical(
+        option, optionx,month=option_months)
+    st.write("Top influencers for each "+ option +" based on subscribers")
     st.write(filter_product)
+    plt.rcdefaults()
+    fig, ax = plt.subplots()
+    ax.barh(filter_product[option], filter_product[optionx], align='center')
+    ax.invert_yaxis()  # labels read top-to-bottom
+    ax.set_xlabel("No. of "+optionx)
+    plt.xticks(rotation=90)
+    st.pyplot(fig)
 
+
+
+    # example visualization 2
     option1 = st.selectbox('Choose category', FILTERS)
     option2 = st.selectbox('Find top Influencers in these subcategories',
-                           instagram.get_category_items(option1))
-
+                        platform.get_category_items(option1))
     # list of N's for top influencers
-
     # default value of N to be displayed in selection box
-    default_topn = TOP_N.index(3)
+    default_topn=TOP_N.index(3)
     option3 = st.selectbox(
-        'Select Number of Influencers to display', TOP_N, index=default_topn)
-    option4 = st.selectbox('Select Metric', instagram.metrics)
+        'Select Number of Influencers to display',TOP_N,index=default_topn)
+    option4 = st.selectbox('Select Metric', platform.metrics)
 
     # get top influencers across a sub-category based on subsrcibers
-    subframe = instagram.get_subcategory_items(instagram.df, option1, option2)
-    dfs = instagram.find_topn_influencers(subframe, option3)[option4]
+    subframe= platform.filter_by_month(platform.df,option_months)
+    subframe = platform.get_subcategory_items(subframe, option1, option2)
+    dfs = platform.find_topn_influencers(subframe, option3)[option4]
     st.write("Top "+str(option3)+" influencers for "+option2 +
-             " under " + option1 + " based on " + option4)
+            " under " + option1 + " based on " + option4)
+    # st.write(pd.DataFrame(zip(dfs['AccountName'],dfs[option4]),columns=['Account Name',option4]))
 
     plt.rcdefaults()
     fig, ax = plt.subplots()
-    y_pos = range(len(dfs[PRIMARY_KEY]))
+    y_pos=range(len(dfs[PRIMARY_KEY]))
     ax.barh(dfs[PRIMARY_KEY], dfs[option4], align='center')
     ax.invert_yaxis()  # labels read top-to-bottom
     ax.set_xlabel("No. of "+option4)
