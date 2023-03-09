8b4f5b9c564c01d520fb5ee7853089832f0ef866
aryanagarwal00 <ara005@ucsd.edu>
2023-03-09 00:34:43 -0800

    rewrote heatmap and pie chart

--- src/data_visualization.py
+++ src/data_visualization.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 from matplotlib_venn import venn2, venn2_circles
 import pandas as pd
 import platforms as pt
-from config import MONTHS, MONTHS_DTYPE, NAME, NUMERIC_COLUMNS, PRIMARY_KEY, PLATFORMS, TOP_N
+from config import MONTHS, MONTHS_DTYPE, NAME, PRIMARY_KEY, PLATFORMS, TOP_N, FILTERS, METRICS
 import numpy as np
 import seaborn as sns
 
@@ -54,11 +54,10 @@ def line_chart(df_social_medias_time, requested_media='Instagram'):
         plt.suptitle(platform + 'Top Influencers Following in 2022')
         plt.text(0.5, 0.02, 'Time', ha='center', va='center')
         plt.text(0.06, 0.5, 'Number of Followers', ha='center', va='center', rotation='vertical')
-        # plt.show()
         fig_count += 1
     return figs
 
-def venn_diagram(df_instagram, df_youtube, months=MONTH[0]):
+def venn_diagram(df_instagram, df_youtube, months=MONTHS[0]):
     '''
     display venn diagram comparing and contrasting countries for instagram and youtube
     param: 
@@ -261,109 +260,126 @@ def bar_CategoryvInfluencers(df_instagram, df_youtube, requested_media='Instagra
         plt.ylabel('Number of Influencers')
     return figs
 
-def heatmap(df_instagram, df_youtube, requested_media='Instagram'):
+def heatmap(df_media: pd.DataFrame, platform: str=PLATFORMS[0], month: str=MONTHS[0]) -> plt.figure():
     '''
     display heat map of the number of subscribers and top influencer for each category in each country for given social media platform
     param: 
-    df_instagram (type: pd.DataFrame) : non-dated DataFrame of influencers on instagram data
-    df_youtube (type: pd.DataFrame) : non-dated DataFrame of influencers on youtube data
-    requested_media (type: string): which social media platform's information to display
+    df_media (type: pd.DataFrame) : UNFILTERED DataFrame of selected PLATFORM and MONTH
+    platform (type: string): platform selected
+    month (type: string): month selected
 
     output:
     seaborn heat map
     '''
-    assert isinstance(df_instagram, pd.DataFrame)
-    assert isinstance(df_youtube, pd.DataFrame)
-    assert isinstance(requested_media, str)
-    platform_options = ['Instagram', 'Youtube']
-    all_dfs = [df_instagram, df_youtube]
-    if requested_media != 'All':
-        all_dfs = [all_dfs[platform_options.index(requested_media)]]
-        platform_options = [requested_media]
-    figs = {}
+
+    # TODO: Test the code
+    # TODO: Allow DataFrame input to not be filtered by a certain metric
+    # TODO: Produce 4 plots for all months, if requested
+    # TODO: Produce 2 plots for all platforms, if requested
+
     fig_count = 0
-    figs[fig_count] = plt.figure(fig_count)
-    for platform in platform_options:
-        df_platform = all_dfs[fig_count][['Influencer name', 'Category', 'Audience country', 'Subscribers']]
-        all_categories = df_platform['Category'].unique()
-        all_countries = df_platform['Audience country'].unique()
-        new_df = pd.DataFrame(columns=all_countries, index=all_categories)
-        labels = [[0 for _ in range(len(all_countries))] for _ in range(len(all_categories))]
-        col = 0
-        for country in all_countries:
-            df_vals = []
-            row = 0
-            for category in all_categories:
-                df_filtered_country = df_platform.loc[df_platform['Audience country'] == country]
-                df_filtered_category = df_filtered_country.loc[df_filtered_country['Category'] == category]
-                if len(df_filtered_category) == 0:
-                    df_vals.append(0)
-                    labels[row][col] = 'N/A'
-                    row += 1
-                    continue
-                highest_following = df_filtered_category.loc[df_filtered_category['Subscribers'].idxmax()]
-                df_vals.append(highest_following['Subscribers'])
-                labels[row][col] = highest_following['Influencer name']
+    figs = plt.figure(fig_count)
+    df_platform = df_media[[PRIMARY_KEY, 'Category_1', 'Country', 'Subscribers']]
+    all_categories = df_platform['Category_1'].unique()
+    all_countries = df_platform['Country'].unique()
+    new_df = pd.DataFrame(columns=all_countries, index=all_categories)
+    labels = [[0 for _ in range(len(all_countries))] for _ in range(len(all_categories))]
+    col = 0
+    figs = plt.figure(fig_count)
+    for country in all_countries:
+        df_vals = []
+        row = 0
+        for category in all_categories:
+            df_filtered_country = df_platform.loc[df_platform['Country'] == country]
+            df_filtered_category = df_filtered_country.loc[df_filtered_country['Category_1'] == category]
+            if len(df_filtered_category) == 0:
+                df_vals.append(0)
+                labels[row][col] = 'N/A'
                 row += 1
-            new_df[country] = df_vals
-            col += 1
-        sns.heatmap(new_df, annot=labels, fmt='')
-        plt.title('Number of Subscribers/Followers for Top Influencers in a Category and Country on ' + platform)
-        fig_count += 1
-        figs[fig_count] = plt.figure(fig_count)
+                continue
+            highest_following = df_filtered_category.loc[df_filtered_category['Subscribers'].idxmax()]
+            df_vals.append(highest_following['Subscribers'])
+            labels[row][col] = highest_following[PRIMARY_KEY]
+            row += 1
+        new_df[country] = df_vals
+        col += 1
+    sns.heatmap(new_df, annot=labels, fmt='')
+    plt.title('Number of Subscribers for Top Influencers in a Category and Country on ' + platform + ' in ' + month + ' 2022')
     return figs
 
-def pie_chart(df_instagram, df_youtube, requested_media='Instagram'):
+def pie_chart(df_media: pd.DataFrame, platform: str=PLATFORMS[0], month: str=MONTHS[0], df_filter: str='Country', metric: str='', category='') -> plt.figure():
     '''
-    display pie chart of the percent of influencers that fall in each categories in each country for given social media platform
+    if filter == 'Country':
+        display pie chart of the percent of influencers that fall in each categories for a given country and social media platform
+    if filter == 'Category_1':
+        display a SINGLE pie chart of the percent each influencer contributes to a METRIC for a given category and social media platform
     param: 
-    df_instagram (type: pd.DataFrame) : non-dated DataFrame of influencers on instagram data
-    df_youtube (type: pd.DataFrame) : non-dated DataFrame of influencers on youtube data
-    requested_media (type: string): which social media platform's information to display
+    df_media (type: pd.DataFrame) : UNFILTERED DataFrame of influencers of selected PLATFORM and MONTH
+    platform (type: string): platform selected
+    month (type: string): month selected
+    df_filter (type: string): independent variable for pie chart
+    metric (type: string): a single country if filter == 'Country', any METRICS value if filter == 'Category_1' (including 'AccountName')
+    category (type: string): a single category if filter == 'Category' from the dataframe
 
     output:
     matplotlib pie chart
     '''
-    assert isinstance(df_instagram, pd.DataFrame)
-    assert isinstance(df_youtube, pd.DataFrame)
-    assert isinstance(requested_media, str)
-    platform_options = ['Instagram', 'Youtube']
-    fig_count = 0
-    all_dfs = [df_instagram, df_youtube]
-    if requested_media != 'All':
-        all_dfs = [all_dfs[platform_options.index(requested_media)]]
-        platform_options = [requested_media]
-    fig_count = 0
-    figs = {}
-    for platform in platform_options:
-        figs[fig_count] = plt.figure(fig_count)
-        df_platform = all_dfs[fig_count][['Influencer name', 'Category','Audience country']]
-        all_countries = df_platform['Audience country'].unique()
-        plots = len(all_countries)
-        row_plots = int(plots**0.5)
-        col_plots = plots // row_plots
-        num = 1
-        for country in all_countries:
-            df_filtered_country = df_platform.loc[df_platform['Audience country'] == country]
-            count_categories = df_filtered_country.groupby('Category')['Audience country'].count()
-            if len(df_filtered_country) == 0:
-                continue
-            category_divisions = count_categories.values
-            category_labels = count_categories.index
-            explode = np.zeros(len(category_labels))
-            max_val = max(category_divisions)
-            max_idx = np.where(category_divisions == max_val)
-            explode[max_idx] = max_val
-            plt.subplot(row_plots,col_plots,num)
-            plt.pie(category_divisions, labels=category_labels, explode=explode/10)
-            plt.title(country)
-            num += 1
-        plt.suptitle('Demographic Division of Product Category by Number of Influencers on ' + platform)
-        fig_count += 1
-    return figs
 
-instagram = pd.read_csv('C:/Users/forMED Technologies/Documents/Github/ece143-social/data/Instagram/social media influencers - instagram.csv')
-youtube = pd.read_csv('C:/Users/forMED Technologies/Documents/Github/ece143-social/data/Instagram/social media influencers - instagram.csv')
-pie_chart(instagram, youtube, 'All').show()
-for fig in figs:
-    fig.show()
+    # TODO: Test the code
+    # TODO: Allow DataFrame input to not be filtered by a certain metric
+    # TODO: Allow user to select a specific country based on DataFrame and pass that into metric field
+    # TODO: Allow user to select a specific category based on DataFrame and pass that into category field
+    # TODO: Produce 4 plots for all months, if requested
+    # TODO: Produce 2 plots for all platforms, if requested
+
+    figs = plt.figure()
+    if df_filter == 'Country':
+        df_platform = df_media[['Category_1', df_filter]]
+        #all_countries = df_platform[df_filter].unique()
+        #plots = len(all_countries)
+        #row_plots = int(plots**0.5)
+        #col_plots = plots // row_plots
+        #col_plots += plots % col_plots
+        #num = 1
+        df_filtered_country = df_platform.loc[df_platform[df_filter] == metric]
+        count_categories = df_filtered_country.groupby('Category_1')[df_filter].count()
+        #if len(count_categories) == 0:
+        #    continue
+        category_divisions = count_categories.values
+        category_labels = count_categories.index
+        explode = np.zeros(len(category_labels))
+        max_val = max(category_divisions)
+        max_idx = np.where(category_divisions == max_val)
+        explode[max_idx] = 0.1
+        #plt.subplot(row_plots,col_plots,num)
+        plt.pie(category_divisions, labels=category_labels, explode=explode)
+        plt.title(metric)
+        # num += 1
+        plt.suptitle('Demographic Division of Product Category by Number of Influencers on ' + platform + ' in ' + month + ' 2022')
+    else:
+        assert metric in METRICS
+        df_platform = df_media[[PRIMARY_KEY, df_filter, metric]]
+        # all_categories = df_platform[df_filter].unique()
+        # plots = len(all_categories)
+        #row_plots = int(plots**0.5)
+        #col_plots = plots // row_plots
+        #col_plots += plots % col_plots
+        #num = 1
+        df_filtered_category = df_platform.loc[df_platform[df_filter] == category]
+        group = df_filtered_category.groupby(PRIMARY_KEY)
+        metric_df = group.apply(lambda x: x[metric].unique())
+            # if len(count_metric) == 0:
+            #    continue
+        metric_divisions = metric_df.values
+        metric_labels = metric_df.index
+        assert len(metric_labels) == len(metric_divisions)
+        explode = np.zeros(len(metric_divisions))
+        max_val = max(metric_divisions)
+        max_idx = np.where(metric_divisions == max_val)
+        explode[max_idx] = 0.1
+        #plt.subplot(row_plots,col_plots,num)
+        plt.pie(metric_divisions, labels=metric_labels, explode=explode)
+        plt.title(category)
+        num += 1
+        plt.suptitle('Product Category Division of Influencers by ' + metric + ' on ' + platform + ' in ' + month + ' 2022')
+    return figs
--- src/platforms.py
+++ src/platforms.py
@@ -1,7 +1,6 @@
-import pandas as pd
-import numpy as np
-from wordhoard import Synonyms
 from collections import Counter
+from pathlib import Path
+from typing import Set
 
 import numpy as np
 import pandas as pd
@@ -12,18 +11,29 @@ from uts import weighted_average
 
 class Social:
 
+    def __init__(self, platform: str):
         '''
         initialize instance of platform
         '''
+        # list of filter categories
+        self.metrics: Set[str] = set()
+        # TODO(PRASAD): approve these calls
+        self.df = self.load_dfs(platform)
+        self.poularity = weighted_average(self.df, 'Subscribers')
 
     def get_categories(self):
         '''
         Returns the categories in a dataframe
         '''
+        return list(self.df.columns)
 
+    def preprocess_frame(self, df: pd.DataFrame) -> pd.DataFrame:
         '''
         clean up dataframe
         '''
+        # remove nan
+        df.fillna('other', axis=0, inplace=True)
+
         # converts string numerics to floats
         for column in df.columns:
             if column in METRICS:
@@ -31,7 +41,20 @@ class Social:
                     self.value_to_float)
                 self.metrics.add(column)
 
+        return df
 
+    def load_dfs(self, media: str) -> pd.DataFrame:
+        assert media in ['Instagram', 'TikTok', 'Youtube']
+        dir_path: Path = Path(__file__).absolute(
+        ).parent.parent / 'data' / media
+        dfs = {str: pd.DataFrame}
+        for mnth in MONTHS:
+            dfs[mnth] = self.preprocess_frame(
+                pd.read_csv(dir_path / f"{media}_{mnth}.csv",
+                            encoding='utf-8'))
+            dfs[mnth]['Month'] = mnth
+            # drop duplicate columns
+            dfs[mnth] = dfs[mnth].drop_duplicates(subset=[PRIMARY_KEY])
 
         df: pd.DataFrame = pd.concat([dfs[mnth]
                                      for mnth in MONTHS], ignore_index=True)
@@ -39,51 +62,44 @@ class Social:
         print(f"A Data Frame of shape {df.shape} formed!!")
         return df
 
+    def get_influencer_fromdf(self, df):
         '''
         pass a dataframe and extract influencer names
         '''
+        return list(df[NAME])
 
     def get_category_items(self, category):
         '''
         gets items pertaining to a category
         param:
         path (type: string) : file name
-
         output:
         df (type: pd.dataframe) : output data frame column
         '''
 
+        assert(category in self.get_categories()), "inavlid category"
+        assert(isinstance(self.df, pd.DataFrame)), "inavlid dataframe"
 
+        # remove nans
         self.df[category] = self.df[category].replace(np.nan, 'other')
 
+        # get entries in category
+        subcategories = list(self.df[category])
 
+        # get subcategories
+        subcategories = Counter(subcategories)
 
         return list(subcategories.keys())
 
-    # def process_subcategories(self, subcategories):
-    
-    #     assert(isinstance(subcategories,list)),"inavlid sub-category list"
-    #     synonyms_dict = {}
-    #     for word in subcategories:
-    #         synonym = Synonyms(word)
-    #         synonyms_results = synonym.find_synonyms()
-    #         if synonyms_results is not None:
-    #             synonyms_dict[word] = synonyms_results
-    #         else:
-    #             synonyms_dict[word] = 'no synonyms found'
-
-
-    #     return synonyms_dict
-    
-    
     def get_subcategory_items(self, df, category, subcategory):
         '''
         gets items pertaining to a sub-category
         param:
         category (type: string) : main category
         subcategory (type: string) : subcategory under the specific category
-
         output:
+        items (type: pd.dataframe) : output data frame
+                column filtered by subcategory
         '''
 
         return df[df[category].str.contains(subcategory)]
@@ -94,7 +110,6 @@ class Social:
         param:
         category (type: string) : main category
         subcategory (type: string) : subcategory under the specific category
-
         output:
         items (type: pd.dataframe) : output data frame
                 column filtered by subcategory
@@ -103,21 +118,28 @@ class Social:
 
         return df[df['Month'].str.contains(month)]
 
+    def find_topn_influencers(self, dataframe, N):
         '''
         returns dataframe pertaining to top N influencers
         param:
         dataframe (type: pd.DataFrame) : dataframe
         N (type: int) : number of influencer data needed
-
         output:
+        dictionary of dataframe (type: pd.DataFrame):
+                sorted top N influencer data
         '''
+        top = {}
+        # return top N influencers based on each metric
         for metric in self.metrics:
+            df = dataframe.sort_values(by=[metric], ascending=False)
+            top[metric] = df.head(N)
 
         return top
 
     def get_topn_influencers_categorical(self,criteria, metric, month=MONTHS[0],N=1):
-
         '''
+        Pass a criteria(category) and get info of
+            top influencers in each subcategory
         '''
         products = self.get_category_items(criteria)
         
@@ -135,9 +157,10 @@ class Social:
                                                 m_df, criteria, product),
                                              1)[metric]], axis=0)
 
-
         return filtered_df
 
+    # helper functions
+    def value_to_float(self, x):
         if type(x) == float or type(x) == int:
             return float(x)
         if 'K' in x:
