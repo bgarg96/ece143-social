baf8f37abdf26b8299c4a3e27d8ad4102663d8f5
pkamath07 <127076454+pkamath07@users.noreply.github.com>
2023-03-10 02:20:23 -0800

    Dev prasad2 (#21)
    * committing platform class and datasets
    
    committing platform class and datasets
    
    * Added scripts for streamlit application
    
    Added scripts for streamlit application
    
    * updated streamlit app with drop down selection
    
    updated streamlit app with drop down selection
    
    * Integrated nested filtering into streamlit app
    
    Integrated nested filtering into streamlit app
    
    * action items + comments
    
    action items + comments
    
    * data cleanup for instagram
    
    data cleanup for instagram
    
    * renamed files
    
    renamed files
    
    * remove extra files
    
    * removing unused file
    
    * Adding dataset changes  (#4) (#5)
    
    * committing platform class and datasets
    
    committing platform class and datasets
    
    * Added scripts for streamlit application
    
    Added scripts for streamlit application
    
    * updated streamlit app with drop down selection
    
    updated streamlit app with drop down selection
    
    * Integrated nested filtering into streamlit app
    
    Integrated nested filtering into streamlit app
    
    * action items + comments
    
    action items + comments
    
    * data cleanup for instagram
    
    data cleanup for instagram
    
    * renamed files
    
    renamed files
    
    * remove extra files
    
    * removing unused file
    
    ---------
    
    Co-authored-by: bgarg96 <126200649+bgarg96@users.noreply.github.com>
    
    * Committing cleaned TikTok dataset
    
    Committing cleaned TikTok dataset
    
    * Deleting june dataset
    
    Deleting june dataset
    
    * Youtube cleaned up
    
    Youtube cleaned up
    
    * changes to include filter sets for numerics and categories
    
    changes to include filter sets for numerics and categories, changed from drop nan to fill nan with 'other' values
    
    * Added default selection to selectbox
    
    Added default selection to selectbox
    
    * commented out test code
    
    commented out test code
    
    * Added platform selection option
    
    Added platform selection option
    
    * changes to include month and merge resolution
    
    changes to include month and merge resolution
    
    * Added expanders and titles in streamlit and 2 new filters
    
    Added expanders and titles in streamlit and 2 new filters
    
    * Update platforms.py
    
    * updated streamlit with pie chart and fixes for visualizations
    
    updated streamlit with pie chart and fixes for visualizations
    
    * minor fix to pie chart
    
    minor fix to pie chart
    
    * more fixes and set default for metrics selectbox
    
    more fixes and set default for metrics selectbox
    
    * Added Venn and fixes to it
    
    Added Venn and fixes to it
    
    * added histogram (needs fixing)
    
    added histogram (needs fixing)
    
    ---------
    
    Co-authored-by: bgarg96 <126200649+bgarg96@users.noreply.github.com>

--- src/data_visualization.py
+++ src/data_visualization.py
@@ -8,7 +8,24 @@ import seaborn as sns
 from config import METRICS, MONTHS, PLATFORMS, PRIMARY_KEY, TOP_N
 from matplotlib_venn import venn2, venn2_circles
 from uts import weighted_average
+import random
 
+
+def get_colors(N):
+    """
+    Color generator
+
+    Args:
+        N (int): number of colors required
+
+    Returns:
+        list[str]: list of color hex values
+    """        
+    hexadecimal_alphabets = '0123456789ABCDEF'
+    color = ["#" + ''.join([random.choice(hexadecimal_alphabets) for j in
+    range(6)]) for i in range(N)]
+
+    return color
 
 # line chart
 def line_chart(df_medias_months: pd.DataFrame,
@@ -50,7 +67,8 @@ def line_chart(df_medias_months: pd.DataFrame,
         plt.xlabel('Month')
         plt.ylabel('Number of ' + metric)
     plt.xticks(range(4), MONTHS)
-    plt.legend(df_medias_weighted_subs[PRIMARY_KEY])
+    plt.yscale('log')
+    plt.legend(df_medias_weighted_subs[PRIMARY_KEY],loc='upper right')
     if df_filter == '':
         plt.title('2022 ' + platform + " Top Influencers' " +
                   metric + ' in the World')
@@ -64,7 +82,7 @@ def line_chart(df_medias_months: pd.DataFrame,
 
 def venn_diagram(df_instagram: pd.DataFrame,
                  df_youtube: pd.DataFrame,
-                 months: str = MONTHS[0]) -> plt.figure():
+                 months: str = MONTHS[0], platform1="Instagram", platform2="Youtube") -> plt.figure():
     '''
     display venn diagram comparing and contrasting countries
         for instagram and youtube
@@ -90,16 +108,17 @@ def venn_diagram(df_instagram: pd.DataFrame,
     figs = plt.figure()
     venn2(subsets=(insta_unique, youtube_unique,
                    len(common_countries)),
-          set_labels=('Instagram', 'Youtube'),
+          set_labels=(platform1, platform2),
           set_colors=('b', 'r'),
           alpha=0.5)
     venn2_circles(
         subsets=(insta_unique,
                  youtube_unique,
                  len(common_countries)))
+    
+
     plt.title(
-        f"Instagram vs Youtube Number \
-            of Different Countries in {months} 2022")
+        f"Instagram vs Youtube Number of Different Countries in {months} 2022")
     return figs
 
 
@@ -376,10 +395,10 @@ def heatmap(df_media: pd.DataFrame,
 
 
 def pie_chart(df_media: pd.DataFrame,
-              platform: str = PLATFORMS[0],
-              month: str = MONTHS[0],
-              df_filter: str = 'Country',
-              metric: str = '', category: str = '') -> plt.figure():
+              platform: str,
+              month: str,
+              df_filter: str,
+              metric: str, category: str) -> plt.figure():
     '''
     if filter == 'Country':
         display pie chart of the percent of influencers that fall
@@ -395,12 +414,18 @@ def pie_chart(df_media: pd.DataFrame,
     df_filter: independent variable for pie chart
     metric: a single country if filter == 'Country',
             any METRICS value if filter == 'Category_1'
-            (including 'AccountName')
     category: a single category if filter == 'Category' from the dataframe
 
     output:
     matplotlib pie chart
     '''
+
+    # TODO: Test the code
+    # TODO: Allow DataFrame input to not be filtered by a certain metric
+    # TODO: Allow user to select a specific country based on DataFrame and pass that into metric field
+    # TODO: Allow user to select a specific category based on DataFrame and pass that into category field
+    # TODO: Produce 4 plots for all months, if requested
+    # TODO: Produce 2 plots for all platforms, if requested
 
     figs = plt.figure()
     if df_filter == 'Country':
@@ -425,9 +450,9 @@ def pie_chart(df_media: pd.DataFrame,
             category_divisions = highest_divisions
         explode = np.zeros(len(category_labels))
         max_val = max(category_divisions)
-        max_idx = np.where(category_divisions == max_val)
+        max_idx = list(np.where(category_divisions == max_val))
         explode[max_idx] = 0.1
-        plt.pie(category_divisions, labels=category_labels, explode=explode)
+        plt.pie(category_divisions, labels=category_labels, explode=explode, autopct='%1.0f%%', pctdistance=1.1, labeldistance=1.2)
         plt.title(metric)
         plt.suptitle(f"Demographic Division of Product Category by\
             Number of Influencers on {platform} in {month} 2022")
@@ -439,19 +464,117 @@ def pie_chart(df_media: pd.DataFrame,
             .loc[df_platform[df_filter] == category]
         group = df_filtered_category.groupby(PRIMARY_KEY)
         metric_df = group.apply(lambda x: x[metric].unique())
-        metric_divisions = metric_df.values
-        metric_labels = metric_df.index
+        metric_divisions = list(metric_df.values)
+        metric_labels = list(metric_df.index)
         assert len(metric_labels) == len(metric_divisions)
+        if len(metric_labels) > 6:
+            sorted_idx = [b[0] for b in sorted(
+                enumerate(metric_divisions), key=lambda i:i[1])]
+            sorted_labels = []
+            for i in sorted_idx:
+                sorted_labels.append(metric_labels[i])
+            metric_labels = sorted_labels[-6:]
+            metric_labels.insert(0, 'Other')
+            sort_category_divisions = sorted(metric_divisions)
+            highest_divisions = sort_category_divisions[-6:]
+            other_vals = sum(sort_category_divisions[:-6])
+            highest_divisions.insert(0, other_vals)
+            metric_divisions = highest_divisions
         explode = np.zeros(len(metric_divisions))
         max_val = max(metric_divisions)
-        max_idx = np.where(metric_divisions == max_val)
-        explode[max_idx] = 0.1
-        plt.pie(metric_divisions, labels=metric_labels, explode=explode)
+        max_idx = list(np.where(metric_divisions == max_val))
+        explode[max_idx[0]] = 0.1
+        plt.pie([arr[0] for arr in metric_divisions], labels=metric_labels, explode=explode,autopct='%1.0f%%', pctdistance=1.1, labeldistance=1.2)
         plt.title(category)
         plt.suptitle(f"Product Category Division of Influencers by\
-                     {metric} on {platform} in {month} 2022")
+                        {metric} on {platform} in {month} 2022")
     return figs
 
+def bar_influencer_type(df_weighted: pd.DataFrame, platform: str=PLATFORMS[0], df_filter: str='United States') -> plt.figure():
+    figs = plt.figure()
+    df_medias = df_weighted['Subscribers_TW_averge']
+    type_of_influencers = ['Nano', 'Micro', 'Macro', 'Mega', 'Celebrities']
+    count_types = [0]*len(type_of_influencers)
+    for influencer_type in type_of_influencers:
+        if influencer_type == 'Nano':
+            count_types[type_of_influencers.index(influencer_type)] = df_medias[df_medias <= 5000].count()
+        elif influencer_type == 'Micro':
+            count_types[type_of_influencers.index(influencer_type)] = df_medias[(df_medias > 5000) & (df_medias <= 20000)].count()
+        elif influencer_type == 'Macro':
+            count_types[type_of_influencers.index(influencer_type)] = df_medias[(df_medias > 20000) & (df_medias <= 100000)].count()
+        elif influencer_type == 'Mega':
+            count_types[type_of_influencers.index(influencer_type)] = df_medias[(df_medias > 100000) & (df_medias <= 1000000)].count()
+        else:
+            count_types[type_of_influencers.index(influencer_type)] = df_medias[df_medias > 1000000].count()
+    plt.bar(type_of_influencers, count_types)
+    plt.title('Distribution of Types of Influencers by Number of Subscribers')
+    plt.xlabel('Types of Influencers')
+    plt.ylabel('Number of Subscribers')
+    return figs
+
+def bar_graph(df_media: pd.DataFrame,
+              platform: str = PLATFORMS[0],
+              month: str = MONTHS[0],
+              df_filter: str = '',
+              metric: str = '') -> plt.figure():
+    '''
+    display bar chart of FILTER independent variable and METRIC dependent variable 
+    param:
+    df_media (type: pd.DataFrame) : FILTERED DataFrame with 2 columns: FILTER, METRIC
+    platform (type: string): platform selected
+    df_filter (type: string): filter selected
+    metric (type: string): metric selected --> represents dependent variable
+
+    output:
+    matplotlib bar chart
+    '''
+
+    # TODO: Test the code
+    # TODO: Check permutations of filter and metric all graph
+
+    figs = plt.figure()
+    sum_dvar = df_media.groupby(df_filter)[metric].sum()
+    num_views = sum_dvar.sort_values(ascending=False).values
+    ivar_names = sum_dvar.sort_values(ascending=False).index
+    figs = plt.figure()
+    plt.bar(ivar_names, num_views)
+    plt.title('Total ' + metric + ' in Each ' + df_filter + ' on ' +
+                platform + ' in ' + month + ' 2022', loc='center', fontsize=12)
+    plt.xlabel(df_filter)
+    plt.ylabel(metric)
+    return figs
+
+def plot_histogram(df_media: pd.DataFrame,
+              platform: str = PLATFORMS[0],
+              month: str = MONTHS[0],
+              df_filter: str = PRIMARY_KEY,
+              metric: str = '', 
+              top_n: int=TOP_N[-1]) -> plt.figure():
+
+    '''
+    display bar chart histogram of METRIC dependent variable for top TOP_N influencers 
+    param:
+    df_media (type: pd.DataFrame) : FILTERED DataFrame with 2 columns: PRIMARY_KEY, METRIC
+    platform (type: string): platform selected
+    df_filter (type: string): PRIMARY_KEY with all influencer names
+    metric (type: string): metric selected --> represents dependent variable
+    top_n (type: int): number of influencers to display
+
+    output:
+    matplotlib bar chart
+    '''
+
+    figs = plt.figure()
+    assert df_filter == PRIMARY_KEY
+    df_media = df_media.sort_values(by=[metric], ascending=False).head(top_n)
+    influencer_names = df_media[df_filter].values
+    dvar = df_media[metric].values
+    plt.bar(influencer_names, dvar)
+    plt.title('Total ' + metric + ' for Top ' + str(top_n) + ' Influencers on ' +
+                platform + ' in ' + month + ' 2022', loc='center', fontsize=12)
+    plt.xlabel('Top ' + str(top_n) + ' ' + platform + ' Influencers')
+    plt.ylabel(metric)
+    return figs
 
 if __name__ == '__main__':
     platform = pt.Social('Instagram')
--- src/streamlit_app.py
+++ src/streamlit_app.py
@@ -2,8 +2,10 @@ import matplotlib.pyplot as plt
 import streamlit as st
 
 import platforms as pt
-from config import FILTERS, MONTHS, PLATFORMS, PRIMARY_KEY, TOP_N
-
+from config import FILTERS, MONTHS, PLATFORMS, PRIMARY_KEY, TOP_N, METRICS
+import data_visualization as dv
+import uts
+st.set_option('deprecation.showPyplotGlobalUse', False)
 if __name__ == '__main__':
 
     st.title('Social Media Influencers Analysis for Businesses')
@@ -26,7 +28,7 @@ if __name__ == '__main__':
             'Find top Influencers in a category', optionx_combo)
 
         # get top influencers across a sub-category based on subsrcibers
-        optionx = st.selectbox('Select Metric: ', platform.metrics)
+        optionx = st.selectbox('Select Metric: ', platform.metrics, index=platform.metrics.index(METRICS[0]))
         filter_product = platform.get_topn_influencers_categorical(
             option, optionx, month=option_months)
 
@@ -35,8 +37,9 @@ if __name__ == '__main__':
         st.write(filter_product)
         plt.rcdefaults()
         fig, ax = plt.subplots()
+        colors= dv.get_colors(len(filter_product[option]))
         ax.barh(filter_product[option],
-                filter_product[optionx], align='center')
+                filter_product[optionx], align='center', color=colors)
         ax.invert_yaxis()  # labels read top-to-bottom
         ax.set_xlabel("No. of "+optionx)
         plt.xticks(rotation=90)
@@ -57,7 +60,7 @@ if __name__ == '__main__':
             'Select Number of Influencers to display',
             TOP_N,
             index=default_topn)
-        option4 = st.selectbox('Select Metric', platform.metrics)
+        option4 = st.selectbox('Select Metric', platform.metrics, index=platform.metrics.index(METRICS[0]))
 
         # get top influencers across a sub-category based on metric
         subframe = platform.filter_by_month(platform.df, option_months)
@@ -68,8 +71,8 @@ if __name__ == '__main__':
 
         plt.rcdefaults()
         fig, ax = plt.subplots()
-        y_pos = range(len(dfs[PRIMARY_KEY]))
-        ax.barh(dfs[PRIMARY_KEY], dfs[option4], align='center')
+        colors= dv.get_colors(len(dfs[PRIMARY_KEY]))
+        ax.barh(dfs[PRIMARY_KEY], dfs[option4], align='center',color=colors)
         ax.invert_yaxis()  # labels read top-to-bottom
         ax.set_xlabel("No. of "+option4)
         ax.set_xscale('log')
@@ -87,7 +90,7 @@ if __name__ == '__main__':
         default_topn = TOP_N.index(3)
         optionN = st.selectbox(
             'Select top N categories to display', optionsN, index=default_topn)
-        optionM = st.selectbox('Select Aggregated Metric', platform.metrics)
+        optionM = st.selectbox('Select Aggregated Metric', platform.metrics, index=platform.metrics.index(METRICS[0]))
         df_cat = platform.get_N_most_popular_cat(platform.filter_by_month(
             platform.df, option_months), optionM, optionN, optionC)
 
@@ -96,8 +99,9 @@ if __name__ == '__main__':
 
         plt.rcdefaults()
         fig, ax = plt.subplots()
+        colors= dv.get_colors(len(df_cat[FILTERS[1]]))
         ax.barh(df_cat[FILTERS[1]],
-                df_cat['Aggregated '+optionM], align='center')
+                df_cat['Aggregated '+optionM], align='center',color=colors)
         ax.invert_yaxis()  # labels read top-to-bottom
         ax.set_xlabel("No. of Aggregated "+optionM)
         ax.set_xscale('log')
@@ -118,7 +122,7 @@ if __name__ == '__main__':
             optionsN2,
             index=default_topn)
         optionM2 = st.selectbox(
-            'Select an Aggregated Metric', platform.metrics)
+            'Select an Aggregated Metric', platform.metrics, index=platform.metrics.index(METRICS[0]))
         df_cat = platform.get_N_most_popular_country(platform.filter_by_month(
             platform.df, option_months), optionM2, optionN2, optionP)
 
@@ -127,9 +131,122 @@ if __name__ == '__main__':
 
         plt.rcdefaults()
         fig, ax = plt.subplots()
+        colors= dv.get_colors(len(df_cat[FILTERS[0]]))
         ax.barh(df_cat[FILTERS[0]],
-                df_cat['Aggregated '+optionM2], align='center')
+                df_cat['Aggregated '+optionM2], align='center',color=colors)
         ax.invert_yaxis()  # labels read top-to-bottom
         ax.set_xlabel("No. of Aggregated "+optionM2)
         ax.set_xscale('log')
         st.pyplot(fig)
+
+
+    # example visualization 5 : Media Capture of Influencers for a Category
+    st.subheader('Media Capture of Influencers for a Category')
+    my_expander4 = st.expander(label="Find top influencers for a category based on total % of influence")
+    with my_expander4:
+        option_selected = st.selectbox('Choose a '+ FILTERS[1]+ " :",
+                               platform.get_category_items(FILTERS[1]))
+        
+        optionM3 = st.selectbox(
+            'Select a Metric :', platform.metrics, index=platform.metrics.index(METRICS[0]))
+
+        # N and metric slection needed 
+        filter_df_month= platform.filter_by_month(platform.df,month=option_months)
+        fig_pie=dv.pie_chart(filter_df_month,
+              option_platforms,
+              option_months,
+              FILTERS[1], metric=optionM3,
+              category=option_selected
+             )
+        
+        st.pyplot(fig_pie)
+
+
+    # example visualization 6 : Get top countries in a particular category
+    st.subheader('Proportion of influencer in each category in a demographic')
+    my_expander5 = st.expander(label="Proportion of influencer in each category in a demographic")
+    with my_expander5:
+        country_selected = st.selectbox('Choose a '+ FILTERS[0]+ " :",
+                               platform.get_category_items(FILTERS[0]))
+
+        filter_df_month= platform.filter_by_month(platform.df,month=option_months)
+        fig_pie2=dv.pie_chart(filter_df_month,
+              option_platforms,
+              option_months,
+              FILTERS[0],
+              country_selected,
+              ""
+             )
+        
+        st.pyplot(fig_pie2)
+
+    # example visualization 7 Line chart
+    st.subheader('Top Influencers Trends')
+    my_expander6 = st.expander(label='Top Influencers Trends')
+    with my_expander6:
+        
+        metric = st.selectbox(
+            'Select a Metric: ', platform.metrics, index=platform.metrics.index(METRICS[0]))
+        optionsNx = [1, 3, 5, 10]
+        default_topn = TOP_N.index(3)
+        optionNx = st.selectbox(
+        'Select N Influencers to display',
+        optionsNx,
+        index=default_topn)
+        df_medias_months = platform.df
+        df_medias_weighted_subs = uts.weighted_average(df_medias_months, metric)
+        fig_lc=dv.line_chart(df_medias_months, df_medias_weighted_subs, platform=option_platforms,metric=metric,top_n=optionNx)
+        st.pyplot(fig_lc)
+
+
+    # example visualization 8 Line chart
+    st.subheader('Analysis of Influencer demograhics')
+    my_expander7 = st.expander(label='Analysis of Influencer demograhics across platforms')
+    with my_expander7:
+        
+
+        platform1 = st.selectbox(
+            'Select Platform 1: ', PLATFORMS, index=0)
+        
+        platform2 = st.selectbox(
+        'Select Platform 2: ', PLATFORMS,index=1)
+
+        month_selection=st.selectbox(
+            'Select Month : ', MONTHS)
+        
+        platform_1= pt.Social(platform1)
+        platform_2= pt.Social(platform2)
+
+        fig_venn=dv.venn_diagram(platform_1.df, platform_2.df, month_selection,platform1,platform2)
+        st.pyplot(fig_venn)
+
+    # example visualization 9: Histogram
+    st.subheader('Top Influencers vs Subscribers')
+    my_expander8 = st.expander(label='Analysis of Top Influencers vs Subscribers')
+    with my_expander8:
+        
+
+        platform7a = st.selectbox(
+            'Select a Platform: ', PLATFORMS, index=0)
+        
+
+        month_selection7=st.selectbox(
+            'Select a Month : ', MONTHS)
+        
+        platform_7a= pt.Social(platform7a)
+
+        optionsN7 = [1, 3, 5, 10]
+        default_topn7 = TOP_N.index(3)
+        optionN7 = st.selectbox(
+        'Select No. of Influencers to display :',
+        optionsN7,
+        index=default_topn7)
+
+        # histogram example
+        df_filter = PRIMARY_KEY
+        metric7 = 'Subscribers'
+        df7 = platform_7a.df
+        month = 'Dec'
+        platform = 'Instagram'
+        fig_hist=dv.plot_histogram(df7, platform7a, month_selection7, df_filter, metric,top_n=optionN7)
+        st.pyplot(fig_hist)
